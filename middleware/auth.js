ff5ec7f626fe0cc4b46d38a5b115d3356589d9eb
Carmen <cdechauri@gmail.com>
2020-06-10 17:04:16 -0700

    Use custom auth middleware instead of password

---
+++ middleware/auth.js
@@ -0,0 +1,24 @@
+const jwt = require('jsonwebtoken');
+const config = require('config');
+
+// you could use password instead of this, if you want to integrate with FB, likedIn
+module.exports = function(req,res,next) {
+
+    const token = req.header('x-auth-token');
+
+    if(!token) {
+        return res.status(400).json( {msg: 'No token, authorization required'});
+    }
+
+    try {
+        const decoded = jwt.verify(token, config.get('jwtSecret'));
+        req.user = decoded.user
+        next();
+
+    }catch (e) {
+        console.error(e.message);
+        res.status(401).send({msg: 'Token is not valid'});
+
+    }
+
+}
--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.get('/', (req,res) => res.send('API running'));
 app.use('/api/users', require('./routes/api/users'));
 // app.use('/api/profile', require('./routes/api/profile'));
 // app.use('/api/posts', require('./routes/api/posts'));
-// app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/auth', require('./routes/api/auth'));
 
 const PORT = process.env.PORT || 5000;
 
