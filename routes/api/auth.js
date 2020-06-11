782c380b77ad08609089a63631f3d71436aff73d
Carmen <cdechauri@gmail.com>
2020-06-10 19:08:43 -0700

    Get profile request

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -43,7 +43,7 @@ router.post(
 
             if (!user) {
                 return res
-                    .status(400)    
+                    .status(400)
                     .json({ errors: [{ msg: 'Invalid Credentials' }] });
             }
 
