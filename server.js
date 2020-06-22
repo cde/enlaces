6de00f23e0cc9f7a1c0af2c19664e459037f0606
Carmen <cdechauri@gmail.com>
2020-06-22 13:28:07 -0700

    Remove build

--- server.js
+++ server.js
@@ -21,9 +21,13 @@ app.use('/api/auth', require('./routes/api/auth'));
 
 // Serve static assets in production
 if(process.env.NODE_ENV === 'production'){
-    app.use(express.static('client/build'));
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname), 'client', 'build','index');
+    // app.use(express.static('client/build'));
+    // app.get('*', (req, res) => {
+    //     res.sendFile(path.resolve(__dirname), 'client', 'build','index');
+    // });
+    app.use(express.static(path.join(__dirname, 'build')));
+    app.get('/', function(req, res) {
+        res.sendFile(path.join(__dirname, 'build', 'index.html'));
     });
 }
 
